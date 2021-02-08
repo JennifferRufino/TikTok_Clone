ade4bd0847b2bf4a05d3fbd0c28d7b8f13066ea3
unknown <guerreiramaya2016@gmail.com>
2021-02-07 21:32:33 -0300

    first commit

---
+++ src/components/Button/index.js
@@ -0,0 +1,12 @@
+import React from 'React';
+
+import EntypoIcons from 'react-native-vector-icons/Entypo';
+import {Button} from './styles';
+
+export default function NewButton() {
+    return (
+        <Button>
+            <EntypoIcons name="plus" size={22} color='#000' />
+        </Button>
+    );
+}
